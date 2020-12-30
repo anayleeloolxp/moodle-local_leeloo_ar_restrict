0a75bdbbe69ccffdd6553de3aedf6f74c96acfff
Anay jain <anayjain@leeloolxp.com>
2020-12-30 13:48:15 +0000

    Fixed js error while doing video

--- lib.php
+++ lib.php
@@ -63,46 +63,17 @@ function local_leeloo_ar_restrict_before_standard_top_of_body_html() {
 
                     $buytext = get_string('buy', 'local_leeloo_ar_restrict');
 
-                    $leeloodiv = "<div class='leeloo_ar_div' id='leeloo_ar_div_$productid'>
-                        <h1 class='leeloo_ar_price'>Paid Activity.</h1>
-                        <a
-                            class='leeloo_ar_cert'
-                            id='leeloo_ar_cert_$productid'
-                            data-toggle='modal'
-                            data-target='#leelooModal_$productid'
-                            href='https://leeloolxp.com/products-listing/product/$urlalias?session_id=$jsessionid'>
-                            $buytext
-                        </a>
-                    </div>";
+                    $leeloodiv = "<div class='leeloo_ar_div' id='leeloo_ar_div_$productid'><h1 class='leeloo_ar_price'>Paid Activity.</h1><a class='leeloo_ar_cert' id='leeloo_ar_cert_$productid' data-toggle='modal' data-target='#leelooModal_$productid' href='https://leeloolxp.com/products-listing/product/$urlalias?session_id=$jsessionid'>$buytext</a></div>";
 
-                    $leeloomodal = "
-                    <div
-                    class='modal fade leeloo_paid_ar_modal'
-                    tabindex='-1'
-                    aria-labelledby='gridSystemModalLabel'
-                    id='leelooModal_$productid'
-                    role='dialog'
-                    style='max-width: 90%;'>
-                        <div class='modal-dialog'>
-                            <div class='modal-content'>
-                                <div class='modal-header'>
-                                    <h4 class='modal-title'>$activityname</h4>
-                                    <button type='button' class='close' data-dismiss='modal'>&times;</button>
-                                </div>
-                                <div class='modal-body'></div>
-                            </div>
-                        </div>
-                    </div>
-                    <style>.leeloo_ar_frame {width: 100%;height: 50vh;border: 0;}</style>
-                    <style>body #region-main,body #region-main.has-blocks{display:none;}</style>";
+                    $leeloomodal = "<div class='modal fade leeloo_paid_ar_modal' tabindex='-1' aria-labelledby='gridSystemModalLabel' id='leelooModal_$productid' role='dialog' style='max-width: 90%;'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><h4 class='modal-title'>$activityname</h4><button type='button' class='close' data-dismiss='modal'>&times;</button></div><div class='modal-body'></div></div></div></div><style>.leeloo_ar_frame {width: 100%;height: 50vh;border: 0;}</style><style>body #region-main,body #region-main.has-blocks{display:none;}</style>";
 
-                    $activityrecord->content = $leeloodiv . $leeloomodal;
-                    $activityrecord->intro = $leeloodiv . $leeloomodal;
+                    $activityrecord->content = '';
+                    $activityrecord->intro = '';
 
                     $PAGE->set_activity_record($activityrecord);
 
-                    $js2 = 'document.getElementById("region-main").style.display = "inline-block";
-                    document.getElementById("region-main").innerHTML = "' . $leeloodiv . $leeloomodal . '";';
+                    $js2 = 'document.getElementById("region-main").style.display = "none";
+                    document.getElementById("region-main").insertAdjacentHTML("afterend", "' . $leeloodiv . $leeloomodal . '")';
 
                     $PAGE->requires->js_init_code("$js2");
                 }
